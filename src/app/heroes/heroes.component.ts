9592b8eaf8657727640a8dda88c3da05b6419cf3
Thi Nga Tran <thnga.tran@gmail.com>
2023-01-27 09:48:44 +0100

    Create a Feature component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,14 +10,12 @@ import { HEROES } from '../Daten/mock-heroes';
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
   selectedHero?: Hero;
-  
+
   constructor() { }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
- 
-
   ngOnInit(): void {
   }
 
